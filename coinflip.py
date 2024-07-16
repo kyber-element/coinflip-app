59e34146da867abea6e52c2f1557a82277663ee1
kyber-element <hatcher.adam.d@gmail.com>
2024-07-16 10:56:34 -0500

    Add files via upload

---
+++ coinflip.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('Tossing a Coin')
+
+st.write('It is not a functional application yet. Under construction.')
+
+def flip_coin():
+    return 'Heads' if random.randint(0, 1) == 0 else 'Tails'
+
+if __name__ == "__main__":
+    result = flip_coin()
+    print(result)

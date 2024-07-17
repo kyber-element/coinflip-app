4cf2faf03d166b4bad903721f2347ee511b0b708
kyber-element <hatcher.adam.d@gmail.com>
2024-07-17 11:27:33 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import random
 
 st.header('Tossing a Coin')
 
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experient of {number_of_trials} trials.')
+
 st.write('It is not a functional application yet. Under construction.')
-
-def flip_coin():
-    return 'Heads' if random.randint(0, 1) == 0 else 'Tails'
-
-if __name__ == "__main__":
-    result = flip_coin()
-    print(result)
